b240f65765fa3afb866d9ed0b00406b3c2d5ee69
Rodrick <rodrick.d@hypertext.com.kw>
2023-12-04 19:48:03 -0700

    Home Screen UI coded

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 import { TouchableOpacity,View, StyleSheet, Dimensions, Text, ScrollView, FlatList, Pressable, Image} from 'react-native'
 import { Icon } from 'react-native-elements';
+import Countdown from 'react-native-countdown-component';
 
 import HomeHeader from './components/HomeHeader';
 import { colors, parameters } from '../../global/styles';
@@ -27,7 +28,7 @@ export default function HomeScreen({}) {
 
                     <View>
 
-                        <View>
+                        <View style={{backgroundColor:colors.cardbackground, paddingBottom: 5}}>
                             <View style={{marginTop: 10, flexDirection: 'row', justifyContent: 'space-evenly'}}>
                                 
                                 <TouchableOpacity
@@ -138,6 +139,20 @@ export default function HomeScreen({}) {
                         </Text>
                     </View>
 
+                    <View style ={{flexDirection: 'row', alignItems: 'center', paddingTop: 15, paddingBottom: -5}}>
+                        <Text style ={{marginLeft: 15, fontSize: 16, marginTop: -10, marginRight: 5}}>
+                            Options changing in
+                        </Text>
+                        <Countdown 
+                            until = {1600}
+                            size={14}
+                            digitStyle={{backgroundColor: colors.lightgreen}}
+                            digitTxtStyle={{color: colors.cardbackground}}
+                            timeToShow={['M', 'S']}
+                            timeLabels={{m:'Min', s: 'Sec'}}
+                        />
+                    </View>
+
                     <View>
                         <FlatList
                             style={{marginTop: 10, marginBottom: 10}}
@@ -162,6 +177,63 @@ export default function HomeScreen({}) {
                         />
                     </View>
 
+                {/* Promotions Section Start */}
+
+                    <View style ={styles.headerTxtView}>
+                            <Text style={styles.headerTxt}>
+                                Promotions Available
+                            </Text>
+                        </View>
+
+                        <View>
+                            <FlatList
+                                style={{marginTop: 10, marginBottom: 10}}
+                                horizontal = {true}
+                                data = {restaurantsData}
+                                keyExtractor={(item, index) => index.toString()}
+                                showsHorizontalScrollIndicator = { false}
+
+                                renderItem = {({item})=>(
+                                    <View style = {{marginRight: 5}}>
+                                        <FoodCard 
+                                            screenWidth={SCREEN_WIDTH*0.8}
+                                            images = {item.images}
+                                            restaurantName = {item.restaurantName}
+                                            farAway={item.farAway}
+                                            businessAddress={item.businessAddress}
+                                            averageReview={item.averageReview}
+                                            numberOfReview={item.numberOfReviews}
+                                        /> 
+                                    </View>
+                                )}
+                            />
+                        </View>
+
+                {/* Promotions Section Ends */}
+
+                <View style ={styles.headerTxtView}>
+                    <Text style={styles.headerTxt}>
+                        Restaurants In Your Area
+                    </Text>
+                </View>
+
+                <View style={{width: SCREEN_WIDTH, paddingTop:10}}>
+                    {
+                        restaurantsData.map(item => (
+                            <View key={item.id} style={{paddingBottom: 20}}>
+                                <FoodCard 
+                                    screenWidth={SCREEN_WIDTH*0.95}
+                                    images = {item.images}
+                                    restaurantName = {item.restaurantName}
+                                    farAway={item.farAway}
+                                    businessAddress={item.businessAddress}
+                                    averageReview={item.averageReview}
+                                    numberOfReview={item.numberOfReviews}
+                                /> 
+                            </View>
+                        ))
+                    }
+                </View>
 
                 </ScrollView>
         </View>
